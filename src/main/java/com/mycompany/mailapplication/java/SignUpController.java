3ed36b2e30fc23a4f3d702a289dc33c2d5bbe3fb
Jeovani Quintanilla <jeova@Jeo-PC>
2025-03-19 21:42:51 -0400

    Feat: Created SignUp GUI

--- src/main/java/com/mycompany/mailapplication/java/SignUpController.java
+++ src/main/java/com/mycompany/mailapplication/java/SignUpController.java
@@ -2,8 +2,30 @@ package com.mycompany.mailapplication.java;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class SignUpController {
+    
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private TextField fnameField;
+
+    @FXML
+    private TextField lnameField;
+
+    @FXML
+    private TextField pwordField;
+
+    @FXML
+    private Label signinText;
+
+    @FXML
+    private Button signupBtn;
+    
 
     @FXML
     private void switchToPrimary() throws IOException {

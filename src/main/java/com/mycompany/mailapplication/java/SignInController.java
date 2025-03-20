41a0be10ee942cd5218035578276ffa5cb328fc5
Jeovani Quintanilla <jeova@Jeo-PC>
2025-03-19 21:24:56 -0400

    Feat: Created SignIn GUI

--- src/main/java/com/mycompany/mailapplication/java/SignInController.java
+++ src/main/java/com/mycompany/mailapplication/java/SignInController.java
@@ -3,11 +3,22 @@ package com.mycompany.mailapplication.java;
 import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class SignInController {
 
+     @FXML
+    private Button SignInBtn;
+
     @FXML
-    private Button primaryButton;
+    private TextField emailField;
+
+    @FXML
+    private TextField pwordField;
+
+    @FXML
+    private Label signupText;
 
     @FXML
     private void switchToSecondary() throws IOException {

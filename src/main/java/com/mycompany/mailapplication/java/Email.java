bfd1f32c29030ce14c6a1ede8d30bb5b2197a162
Jeovani Quintanilla <jeova@Jeo-PC>
2025-03-21 00:42:13 -0400

    Docs: Added JavaDoc to Email class

--- src/main/java/com/mycompany/mailapplication/java/Email.java
+++ src/main/java/com/mycompany/mailapplication/java/Email.java
@@ -5,8 +5,8 @@
 package com.mycompany.mailapplication.java;
 
 /**
- *
- * @author jeova
+ * Purpose: this class defines a email
+ * @author jeovani quintanilla
  */
 public class Email {
     
@@ -15,6 +15,13 @@ public class Email {
     private String subject;
     private String message;
 
+    /**
+     * Email Constructor 
+     * @param sender
+     * @param recepient
+     * @param subject
+     * @param message
+     */
     public Email(String sender, String recepient, String subject, String message) {
         this.sender = sender;
         this.recepient = recepient;
@@ -22,36 +29,72 @@ public class Email {
         this.message = message;
     }
 
+    /**
+     * Email Constructor 
+     */
     public Email() {
     }
     
+    /**
+     * Sender Setter
+     * @param sender
+     */
     public void setSender(String sender) {
         this.sender = sender;
     }
+
+    /**
+     * Sender Getter
+     * @return
+     */
     public String getSender() {
         return sender;
     }
 
-    
+    /**
+     * Recipient Setter
+     * @param recepient
+     */
     public void setRecepient(String recepient) {
         this.recepient = recepient;
     }
+
+    /**
+     * Recipient Getter
+     * @return
+     */
     public String getRecepient() {
         return recepient;
     }
 
-   
+    /**
+     * Subject Setter
+     * @param subject
+     */
     public void setSubject(String subject) {
         this.subject = subject;
     }
+
+    /**
+     * Subject Getter
+     * @return
+     */
     public String getSubject() {
         return subject;
     }
     
-    
+    /**
+     * Message Setter
+     * @param message
+     */
     public void setMessage(String message) {
         this.message = message;
     }
+
+    /**
+     * Message Getter 
+     * @return
+     */
     public String getMessage() {
         return message;
     }

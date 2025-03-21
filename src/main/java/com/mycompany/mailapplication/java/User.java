e86dcc0f2399d0b8999492f658fee0892eec2aa8
Jeovani Quintanilla <jeova@Jeo-PC>
2025-03-21 00:27:13 -0400

    Docs: Added JavaDoc to User Class

--- src/main/java/com/mycompany/mailapplication/java/User.java
+++ src/main/java/com/mycompany/mailapplication/java/User.java
@@ -6,7 +6,8 @@ package com.mycompany.mailapplication.java;
 
 /**
  *
- * @author jeova
+ * @author jeovani quintanilla
+ * Purpose: this class defines a user
  */
 public class User {
     private String fName;
@@ -14,42 +15,87 @@ public class User {
     private String emailAddr;
     private String password;
  
- public User(String fName, String lName, String emailAddr, String password) {
+    /**
+     * @param fName
+     * @param lName
+     * @param emailAddr
+     * @param password
+     * user Constructor 
+     */
+    public User(String fName, String lName, String emailAddr, String password) {
         this.fName = fName;
         this.lName = lName;
         this.emailAddr = emailAddr;
         this.password = password;
     }
 
+    /**
+     * user Constructor
+     */
     public User() {
     }
     
+    /**
+     * First Name Setter 
+     * @param fName
+     */
     public void setfName(String fName) {
         this.fName = fName;
     }
+
+    /**
+     * First Name Getter
+     * @return
+     */
     public String getfName() {
         return fName;
     }
 
+    /**
+     * Last Name Setter
+     * @param lName
+     */
     public void setlName(String lName) {
         this.lName = lName;
     }
+
+    /**
+     * Last Name Getter
+     * @return
+     */
     public String getlName() {
         return lName;
     }
 
+    /**
+     * Email Address Setter
+     * @param emailAddr
+     */
     public void setEmailAddr(String emailAddr) {
         this.emailAddr = emailAddr;
     } 
+
+    /**
+     * Email Address Getter
+     * @return
+     */
     public String getEmailAddr() {
         return emailAddr;
     }
 
-   
+    /**
+     * Password Setter
+     * @param password
+     */
     public void setPassword(String password) {
         this.password = password;
     }    
-     public String getPassword() {
+
+    /**
+     * Password Getter
+     * @return
+     */
+    public String getPassword() {
         return password;
     }
 }
